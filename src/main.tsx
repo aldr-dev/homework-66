aa4e8bae33369d3ada09d42169ca7add9000aa81
Gordus Alexander <aldrmiller@gmail.com>
2024-07-08 18:39:45 +0600

    инициализировал приложение на react + typescript

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+ <BrowserRouter>
+   <ToastContainer />
+   <App/>
+ </BrowserRouter>
+);
